3272bd4b670829340dc25bea13c85e98e3e8eabd
NhanIsBi <92627042+NhanIsBi@users.noreply.github.com>
2022-12-04 20:14:58 +0700

    nhan

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -25,7 +25,7 @@ const upload = multer({
   storage: multerS3({
     acl: 'public-read',
     s3,
-    bucket: 'cloudfront-upload', 
+    bucket: 'nhanangular', 
     key: function(req, file, cb) {
       /*I'm using Date.now() to make sure my file has a unique name*/
       req.file = Date.now() + file.originalname;
