14d794571bdb82b4a3e03f9865c56e178acab44e
NhanIsBi <92627042+NhanIsBi@users.noreply.github.com>
2022-12-04 16:12:07 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
 const upload = require('./middleware/file-upload');
+const express = require("express");
 
+app.get("/", function (req, res) {
+  res.send("IT WORKED");
+});
+app.get("/new", function (req, res) {
+  res.send("New also worked");
+});
+
+app.listen(process.env.PORT || 5000);
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -11,6 +20,3 @@ app.post('/api/v1/upload', upload.array('image', 1), (req, res) => {
   res.send({ image: req.file });
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000!');
-});
