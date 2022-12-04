412fd9ef0d10c21697515c1ee0cd5566cda2b116
NhanIsBi <92627042+NhanIsBi@users.noreply.github.com>
2022-12-04 16:47:23 +0700

    as

--- ng-upload/src/app/image-upload.service.ts
+++ ng-upload/src/app/image-upload.service.ts
@@ -1,14 +1,14 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ImageUploadService {
   constructor(private http: HttpClient) {}
 
   imageUpload(imageForm: FormData) {
-    console.log('image uploading');
-    return this.http.post('http://localhost:3000/api/v1/upload', imageForm);
+    console.log("image uploading");
+    return this.http.post("https://test.jobhere.tech/api/v1/upload", imageForm);
   }
 }
